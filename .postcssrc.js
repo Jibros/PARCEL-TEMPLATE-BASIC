d274bc72c51a8da30e8680c1020a5091bb1de139
지승호 <122860306+seunghoji@users.noreply.github.com>
2024-10-23 10:38:30 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM : ECMA SCRIPT MODULE
+// CommonJS
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// export {
+//     plugins: [
+//         autoprefixer
+//     ]
+// }
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+}
